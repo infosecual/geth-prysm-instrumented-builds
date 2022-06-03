9a02f5db789c92a5f5b5436b21386e35045e9bb1
David Theodore <prodigalsonsolutions@gmail.com>
2022-06-03 00:51:29 -0600

    update prysm to develop (voting issue fix)

--- prysm/build-env.sh
+++ prysm/build-env.sh
@@ -1,2 +1,2 @@
-BUILDKIT=1 docker build -t instrumented-prysm-bn -f beacon-node.Dockerfile .
+BUILDKIT=1 docker build -t instrumented-prysm-bn -f beaconnode.Dockerfile .
 BUILDKIT=1 docker build -t instrumented-prysm-vc -f validator.Dockerfile .
